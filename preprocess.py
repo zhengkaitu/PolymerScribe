17a293b9d475f604ca158d91615b709b22f402b4
Zhengkai Tu <ztu@mit.edu>
2025-07-15 12:55:24 -0400

    add corrections for stereo bond types and superatoms

--- preprocess.py
+++ preprocess.py
@@ -5,7 +5,8 @@ import json
 import numpy as np
 import os
 from rdkit import Chem
-from typing import Dict
+from SmilesPE.pretokenizer import atomwise_tokenizer
+from typing import Dict, List, Tuple
 
 
 def get_args():
@@ -15,16 +16,49 @@ def get_args():
     return parser.parse_args()
 
 
+def parse_mol_file(mol_file: str) -> List[Tuple[int, int, int]]:
+    # RDKit does not track BEGINWEDGE/BEGINDASH by default
+    # need to manually parse this info from molfile, like in MolScribe
+    with open(mol_file) as f:
+        mol_data = f.read()
+        lines = mol_data.split('\n')
+        stereo_bonds = []
+
+        for i, line in enumerate(lines):
+            if line.endswith("V2000"):
+                tokens = line.split()
+                num_atoms = int(tokens[0])
+                num_bonds = int(tokens[1])
+                for atom_line in lines[i+1:i+1+num_atoms]:
+                    # atom_tokens = atom_line.strip().split()
+                    # coords.append([float(atom_tokens[0]), float(atom_tokens[1])])
+                    pass
+                for bond_line in lines[i+1+num_atoms:i+1+num_atoms+num_bonds]:
+                    bond_tokens = bond_line.strip().split()
+                    start, end, bond_type, stereo = [int(token) for token in bond_tokens[:4]]
+                    if bond_type == 1:
+                        if stereo == 1:
+                            etype = 5
+                            stereo_bonds.append((start - 1, end - 1, etype))
+                        if stereo == 6:
+                            etype = 6
+                            stereo_bonds.append((start - 1, end - 1, etype))
+                break
+
+    return stereo_bonds
+
+
 def _get_row(png_fn: str) -> Dict[str, str]:
     png_path = png_fn
     mol_path = f"{png_fn[:-4]}.corrected.mol"
 
-    assert os.path.exists(png_path)
-    assert os.path.exists(mol_path)
+    assert os.path.exists(png_path), png_path
+    assert os.path.exists(mol_path), mol_path
 
     # Disable sanitization to suppress aromatic labeling by rdkit.
     # The data provider should ensure the correctness of molfiles.
     mol = Chem.MolFromMolFile(mol_path, sanitize=False, removeHs=False)
+    # Chem.SanitizeMol(mol, sanitizeOps=Chem.SANITIZE_ALL ^ Chem.SANITIZE_KEKULIZE)
     # Chem.Kekulize(mol)
     # raw_smi = Chem.MolToSmiles(mol, kekuleSmiles=True, canonical=False)
     raw_smi = Chem.MolToSmiles(mol, kekuleSmiles=False, canonical=False)
@@ -33,28 +67,63 @@ def _get_row(png_fn: str) -> Dict[str, str]:
     # print(reordered_atom_i)
     # print(inverse_map)
 
-    # for now SMILES is the same as raw_SMILES which suffices for the SI images
-    # TODO: extension for R groups may be added later; we'll see
     smi = raw_smi
-    conf = mol.GetConformer()
+    superatoms = {}
+    for atom_idx, atom in enumerate(mol.GetAtoms()):
+        alias = atom.GetPropsAsDict().get("molFileAlias")
+        if alias:
+            superatoms[inverse_map[atom_idx]] = alias
 
+    # copied over from molscribe
+    if superatoms:
+        tokens = atomwise_tokenizer(smi)
+        atom_idx = 0
+        for i, t in enumerate(tokens):
+            if t.isalpha() or t[0] == '[' or t == '*':
+                if atom_idx in superatoms:
+                    symbol = superatoms[atom_idx]
+                    tokens[i] = f"[{symbol}]"
+                atom_idx += 1
+        smi = "".join(tokens)
+
+    conf = mol.GetConformer()
     node_coords = []
     for atom_i in reordered_atom_i:
         coord = conf.GetAtomPosition(atom_i)
         node_coords.append([coord.x, coord.y])
 
     edges = {}
-    for bond_i, bond in enumerate(mol.GetBonds()):
-        # the SI images have no chirality
-        # TODO: extension for stereochemistry may be added later
+    stereo_bonds = parse_mol_file(mol_path)
+    stereo_bonds = {
+        (start, end): bond_type
+        for start, end, bond_type in stereo_bonds
+    }
 
+    for bond_i, bond in enumerate(mol.GetBonds()):
         bond_type = bond.GetBondTypeAsDouble()
         try:
-            assert bond_type.is_integer()
+            assert bond_type.is_integer() or bond_type == 1.5
         except AssertionError:
             print(f"{bond_type}, {mol_path}")
             break
-        bond_type = int(bond_type)
+
+        if bond_type == 1.5:
+            bond_type = 4
+        else:
+            bond_type = int(bond_type)
+
+        # if bond_type == 1:
+        #     bond_dir = bond.GetBondDir()
+        #     if bond_dir:
+        #         print([bond_dir])
+        #     if bond.GetBondDir() == Chem.BondDir.BEGINWEDGE:
+        #         bond_type = 5
+        #     elif bond.GetBondDir() == Chem.BondDir.BEGINDASH:
+        #         bond_type = 6
+
+        if bond_type == 2:
+            if bond.GetStereo() == Chem.BondStereo.STEREOANY:
+                bond_type = 7
 
         # begin_atom_i = bond.GetBeginAtomIdx()
         # end_atom_i = bond.GetEndAtomIdx()
@@ -67,6 +136,12 @@ def _get_row(png_fn: str) -> Dict[str, str]:
         #       f"symbol: {mol.GetAtomWithIdx(begin_atom_i_in_mol).GetSymbol()} "
         #       f"end i: {bond.GetEndAtomIdx()} -> {end_atom_i}, "
         #       f"symbol: {mol.GetAtomWithIdx(end_atom_i_in_mol).GetSymbol()}")
+
+        # Override with type 5 and 6, if in stereo_bonds
+        bond_type = stereo_bonds.get(
+            (begin_atom_i_in_mol, end_atom_i_in_mol),
+            bond_type
+        )
 
         edge = {
             "begin_atom_i": begin_atom_i,
@@ -101,7 +176,7 @@ def _get_row(png_fn: str) -> Dict[str, str]:
         # lastly, assuming CONNECT and LABEL are attached with the last bracket
         bracket_tokens.append(["<bra>"] + [token for token in SCN])
         bracket_coords.append([brackets[-1][0].x, brackets[-1][0].y])
-        bracket_tokens.append(["<ket>"] + [token for token in SMT])
+        bracket_tokens.append(["<ket>"] + [token for token in str(SMT)])
         bracket_coords.append([brackets[-1][1].x, brackets[-1][1].y])
 
     edges = [[
@@ -156,7 +231,7 @@ def aggregate_into_csv(args) -> None:
                         rows.append(row)
                 else:
                     png_fn = line.strip()
-                    assert png_fn.endswith(".png")
+                    assert png_fn.endswith(".png"), png_fn
                     row = _get_row(png_fn=png_fn)
                     rows.append(row)
 
