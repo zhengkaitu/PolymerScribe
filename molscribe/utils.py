6d6b66911300e761ca160a2843d86d06d4899927
Zhengkai Tu <ztu@mit.edu>
2025-09-16 08:43:17 -0400

    increase max_len for indexing

--- molscribe/utils.py
+++ molscribe/utils.py
@@ -24,7 +24,7 @@ FORMAT_INFO = {
     },
     "nodes": {"max_len": 384},
     "atomtok_coords": {"max_len": 480},
-    "chartok_coords": {"max_len": 480}
+    "chartok_coords": {"max_len": 640}
 }
 
 
